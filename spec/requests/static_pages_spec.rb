0d826b0d14fb490a270b81e68996e5df02657752
santiago_r93@hotmail.com <mariohernandezv@yahoo.com.ar>
2014-10-14 21:39:49 -0500

    acortar static pages y qsq+

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,42 +1,32 @@
 require 'spec_helper'
 
 describe "StaticPages" do
-  describe "Pagina de Inicio (Home)" do
-    it "debe tener el contenido 'Sample App' " do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      visit '/static_pages/home'
-      expect(page).to have_content('Sample App')
-    end
+  subject { page }
 
-it "debe tener el titulo 'Predefinido'" do
-	visit '/static_pages/home'
-	expect(page).to have_title('Sample App / Pagina de inicio')
-	end
+  describe "Pagina de Inicio (Home)" do
+    before { visit root_path}
+    it {should have_content('Sample App')}
+    it {should_not have_title('Sample App / Pagina de inicio')}
+
   end
   
+
+  
   describe "Pagina de ayuda (Help)" do
-  	it "debe tener el contenido 'Help'" do
-  		visit '/static_pages/help'
-  		expect(page).to have_content('Ayuda')
-  	end
-
-
-it "debe tener el titulo 'Sample App / Pagina de ayuda'" do
-	visit '/static_pages/help'
-	expect(page).to have_title('Sample App / Pagina de ayuda')
-	end
+    before {visit ayuda_path}
+    it {should have_content('Help')}
+    it {should have_title('Sample App / Pagina de ayuda')}
 
   end
-  describe "Pagina Acerca de" do
-  	it "debe tener el contenido 'Acerca de'" do
-  		visit '/static_pages/acerca_de'
-  		expect(page).to have_content('Acerca de')
-  	end
 
-  	it "debe tener el titulo 'Sample App / Pagina Acerca de'" do
-	visit '/static_pages/acerca_de'
-	expect(page).to have_title('Sample App / Pagina Acerca de')
-	end
+
+
+
+  describe "Pagina Acerca de" do
+    before{visit acerca_de_path}
+    it {should have_content('Acerca de')}
+    it {should have_title('Sample App / Pagina Acerca de')}
+
   end
 
 end
